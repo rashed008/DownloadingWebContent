2b28a88e06d1f706416424a99d99e82dad6406b7
RASHED <hello@MacBooks-MacBook-Pro.local>
2017-06-04 22:18:36 +0600

    first commit

--- Downloading Web Content/ViewController.swift
+++ Downloading Web Content/ViewController.swift
@@ -10,9 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var webview: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+       /*
+        let url = URL(string: "http://www.stackoverflow.com")!
+        //webview.loadRequest(URLRequest(url: url))
+        webview.loadHTMLString("<h1>Hello World</h1>", baseURL: nil)
+ 
+      */
+        
+        if let url = URL(string: "https://www.stackoverflow.com"){
+        let request = NSMutableURLRequest(url : url)
+            let task = URLSession.shared.dataTask(with: request as URLRequest) {
+             data, response, error in
+                
+                if error != nil {
+                  print(error)
+                } else {
+                
+                    if let unwrapedData = data {
+                        let dataString = NSString(data : unwrapedData, encoding: String.Encoding.utf8.rawValue)
+                        
+                        print(dataString)
+                        
+                        DispatchQueue.main.sync(execute: {
+                        
+                           //update ui
+                        })
+                    }
+                }
+            
+            }
+            task.resume()
+            
+        }
+        
+        print ("hi there")
     }
 
     override func didReceiveMemoryWarning() {
